dc93f277ee291a3fdcf489ec049b79e54c35aa09
ayubxon13 <yuldoshevv7@gmail.com>
2024-06-29 20:34:47 +0500

    added secretKey

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
 
 const isProtected = createRouteMatcher(["(/.*)"]);
 
-const middlewareHandler = (auth: () => { protect: () => void }, req: any) => {
-  try {
-    if (isProtected(req)) {
-      auth().protect();
-    }
-  } catch (error) {
-    console.error("Error in Clerk middleware:", error);
-    throw error; // Rethrow the error to ensure Vercel logs it
+export default clerkMiddleware((auth, req) => {
+  if (isProtected(req)) {
+    auth().protect();
   }
-};
-
-export default clerkMiddleware(middlewareHandler);
+}, {
+  secretKey: process.env.CLERK_SECRET_KEY
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
